5e0c29260b0ed6877bf63e80e48f38f48e808491
Mark Lavrentyev <lavrema@outlook.com>
2024-03-04 09:52:58 -0800

    Write IdentityT

--- src/main/scala/transformers.scala
+++ src/main/scala/transformers.scala
@@ -4,9 +4,21 @@ import monads.*
 import monads.given
 
 // ================ IdentityT ===================
+
+type IdentityT[M[_], A] = M[Identity[A]]
+
+given [M[_]](using Monad[M]): Monad[[A] =>> IdentityT[M, A]] with
+  override def pure[A](a: A): IdentityT[M, A] = pure(a)
+
+  override def map[A, B](ma: IdentityT[M, A])(f: A => B): IdentityT[M, B] = map(ma)(f)
+
+  override def flatMap[A, B](ma: IdentityT[M, A])(f: A => IdentityT[M, B]): IdentityT[M, B] =
+    flatMap(ma)(f)
+
 // ================ OptionT ===================
 // ================ ListT ===================
 // ================ ReaderT ===================
+
 // ================ WriterT ===================
 // ================ StateT ===================
 // ================ ContinuationT ===================
