a19898b1404327678fb2c7771d32bbce8823d2b1
Mark Lavrentyev <lavrema@outlook.com>
2024-03-04 16:24:34 -0800

    Work on transformers

--- src/main/scala/transformers.scala
+++ src/main/scala/transformers.scala
@@ -3,19 +3,19 @@ package monads.transformers
 import monads.*
 import monads.given
 
+trait MonadTrans[T[_[_], _]]:
+  def lift[M[_], A](ma: M[A]): T[M, A]
+
 // ================ IdentityT ===================
 
 type IdentityT[M[_], A] = M[Identity[A]]
 
-given [M[_]](using Monad[M]): Monad[[A] =>> IdentityT[M, A]] with
-  override def pure[A](a: A): IdentityT[M, A] = pure(a)
-
-  override def map[A, B](ma: IdentityT[M, A])(f: A => B): IdentityT[M, B] = map(ma)(f)
-
-  override def flatMap[A, B](ma: IdentityT[M, A])(f: A => IdentityT[M, B]): IdentityT[M, B] =
-    flatMap(ma)(f)
+given MonadTrans[IdentityT] with
+  override def lift[M[_], A](ma: M[A]): IdentityT[M, A] = ma
 
 // ================ OptionT ===================
+
+
 // ================ ListT ===================
 // ================ ReaderT ===================
 
